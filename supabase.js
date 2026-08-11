9eea41b26e6f061ca65f86a30dcaee9f43a54bc0
yamassociates007-design <yamassociates007@gmail.com>
2026-08-12 03:06:21 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,11 @@
 
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://ttrifwaoyjamowgtoygc.supabase.co';
+
+const supabaseAnonKey = 'sb_publishable_OPrSBhIYl1xVbd3AyIl4wg_rPO1pDEn';
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
